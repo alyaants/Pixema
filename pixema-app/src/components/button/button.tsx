9f393db13afcba0e2ef2d626a8bd158d319b7349
Alya Antsiperovich <antsiperovich2002@mail.ru>
2024-01-12 01:03:09 +0300

    added inputs, buttons, icons, arrow

---
+++ pixema-app/src/components/button/button.tsx
@@ -0,0 +1,28 @@
+import { ReactElement } from "react";
+import classNames from "classnames";
+import styles from "./button.module.scss";
+export enum ButtonTypes {
+  Primary = "primary",
+  Secondary = "secondary",
+  Error = "error",
+}
+interface ButtonProps {
+  type: ButtonTypes;
+  title: string | ReactElement;
+  onClick: () => void;
+  disabled?: boolean;
+  className?: string;
+}
+const Button = (props: ButtonProps) => {
+  const buttonStyle = styles[props.type];
+  return (
+    <div
+      onClick={props.onClick}
+      className={classNames(buttonStyle, props.className,{ [styles.disabled]: props.disabled })}
+    >
+      {props.title}
+    </div>
+  );
+};
+
+export default Button;
