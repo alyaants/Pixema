9be49c4e0c2e6f670f5b295c922e2b5a384b88c2
Alya Antsiperovich <antsiperovich2002@mail.ru>
2024-01-15 14:26:14 +0300

    added logo

--- pixema-app/src/App.tsx
+++ pixema-app/src/App.tsx
@@ -1,8 +1,5 @@
 import './App.css';
-import Search from './components/search/search';
 import NewPassword from './pages/newPassword/newPassword';
-import SignIn from './pages/signIn/signIn';
-import SignUp from './pages/signUp/signUp';
 
 function App() {
   return (
